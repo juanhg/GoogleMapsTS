5ec0a5068f85866605f972ac6cb7ad8acc944291
Juan Hernández <Juan.Hernandez@unit4.com>
2016-07-20 10:30:25 +0200

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {  
+  entry: './src/app.ts',
+  output: {
+    filename: './build/app.js'
+  },
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.ts$/, loader: 'ts-loader' }
+    ]
+  }
+}
